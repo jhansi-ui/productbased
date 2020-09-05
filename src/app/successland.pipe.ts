34abbfa2636c33ac145cf3aa12254c92e5d21e32
mamatha <mamathach66@gmail.com>
2020-09-05 12:19:10 +0530

    secondstage

--- src/app/successland.pipe.ts
+++ src/app/successland.pipe.ts
@@ -5,61 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SuccesslandPipe implements PipeTransform {
 
-//   transform(items: any, max_price: string,launchsucessfilters?:any): any {
-//     if(max_price === ''){
-//       return items;
-//     }
-   
-//    else{
-     
-//       console.log(items);
-//       console.log(max_price);
-//     const playersArray:any[]=[];
-//     //const playersArrays:any[]=[];
-    
-//     for (let i=0;i< items.length;i++){
-//       let employeeNames:string=items[i].launch_year;
-//       //let employeeNamess:string=items[i].launch_success;
-//       if(employeeNames.startsWith(max_price)){
-//       playersArray.push(items[i]);
-//       }
-    
-//     }
-//     return playersArray 
-//   }
-
-// }
-transform(items: any, max_price: any, depature_city: any): any {
-    
-  var filtereditems : any = [];
-  // Filter each
-  
-     if(max_price === ''){
+transform(items: any, nameSearch: any, emailSearch: any, companySearch: any){
+  if (items && items.length){
+      return items.filter(item =>{
+          if (nameSearch && item.launch_year.toLowerCase().indexOf(nameSearch.toLowerCase()) === -1){
+              return false;
+          }
+          if (emailSearch && item.launch_success.toLowerCase().indexOf(emailSearch.toLowerCase()) === -1){
+            return items ? "true" : "false";
+        }
+          if (companySearch && item.land_success.toLowerCase().indexOf(companySearch.toLowerCase()) === -1){
+            return items ? "Yes" : "No";
+          }
+          return true;
+     })
+  }
+  else{
       return items;
-    }
-    else if(max_price){
-      console.log(items);
-      console.log(max_price);
-    const playersArray:any[]=[];
-    //const playersArrays:any[]=[];
-    
-    for (let i=0;i< items.length;i++){
-      let employeeNames:string=items[i].launch_year;
-      let launchsucessdata:string=items[i].launch_success;
-      
-      if(employeeNames.startsWith(max_price)){
-      filtereditems.push(items[i]);
-      }
-    //   else if(launchsucessdata.startsWith(depature_city)){
-    //     console.log("departure city",depature_city);
-    //    filtereditems =  items.some(t=>  t.launch_success.includes(depature_city));
-    //  }
-    }
-  
- 
-
-  console.log("filtered",filtereditems);
-   return filtereditems;
-}
+  }
 }
 }
